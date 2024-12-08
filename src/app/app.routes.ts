bddb41a7880cf1376c0b9a8fe6c739c7447c0aa8
3kri11 <esad.mujacevic2003@gmail.com>
2024-12-08 19:22:20 +0100

    Sve uradeno top

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { Routes,RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProductsComponent } from './products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+    {path:'about',component:AboutComponent},
+    {path:'products',component:ProductsComponent},
+    { path: '**', redirectTo: '', pathMatch: 'full' }
+
+];
+@NgModule({
+    imports:[RouterModule.forRoot(routes)],
+    exports:[RouterModule]
+})
+
+export class AppRoutingModule{}
